34e90a9a940352475aa3316af9c2eacaffcd5f26
btskinner <b.skinner@vanderbilt.edu>
2016-12-10 17:21:29 -0600

    init files

---
+++ README.md
@@ -0,0 +1,81 @@
+# Batch download IPEDS
+
+## Purpose
+
+Use this script to batch download [Integrated Postsecondary Education Data System (IPEDS)](http://nces.ed.gov/ipeds/) files. The downloaded files are not unzipped or processed in any way. This script simply saves you the trouble of having to point and click your way through the data center.
+
+Only those files listed in `ipeds_file_list.txt` will be downloaded. The default behavior is to download each of the following files into their own subdirectories: 
+ 
+1. Data file  
+2. Dictionary file
+
+You can also choose to download other data versions and/or program files:  
+  
+1. Data file (STATA version)  
+2. STATA program file (default if you ask for STATA version data)  
+3. SPSS program file  
+4. SAS program file
+
+### IMPORTANT NOTE
+
+The default behavior is to download **ALL OF IPEDS**. If you don't want everything, modify `ipeds_file_list.txt` to include only those files that you want. Simply erase those you don't want, keeping one file name per row.
+
+## To Run
+
+For the default options, simply run the script `downloadipeds.R`. It will create subdirectories as needed to store data and program files. You may wish to place the folder in its own directory. Just make sure that `ipeds_file_list.txt` is in the same directory. For example, let's say you place both files in a directory called `ipeds`:
+
+```
+./ipeds
+|__ downloadipeds.R
+|__ ipeds_file_list.txt
+```
+
+If you run it with the default options, two new subdirectories will be created, one for the `data` files and one for the `dictionary` files. Using the above example, your directory will look like this after it is finished:
+
+```
+./ipeds
+|__ downloadipeds.R
+|__ ipeds_file_list.txt
+|__ /data
+|   |__ HD2015.zip
+|   |__ IC2015.zip
+|   |__ <...>
+|
+|__ /dictionary
+    |__ HD2015_Dict.zip
+    |__ IC2015_Dict.zip
+    |__ <...>
+```
+
+To download other program scripts or Stata versions of the data, change the following commands in `downloadipeds.R` from `FALSE` to `TRUE`:
+
+```
+## -----------------------------------------------------------------------------
+## CHOOSE WHAT YOU WANT (TRUE == Yes, FALSE == No)
+## -----------------------------------------------------------------------------
+
+## default
+primary_data = TRUE
+dictionary = TRUE
+
+## STATA version
+## (NB: downloading Stata version of data will also get Stata program files)
+stata_data = FALSE
+
+## other program files
+prog_spss = FALSE
+prog_sas  = FALSE
+```
+
+## Data size
+
+As of 10 December 2016, downloading all IPEDS files (setting all options to 	`TRUE`) requires approximately 1.5 GB of disk space. Granted, you probably don't need both regular and Stata versions of the data files (which are the bulk of the directory size). Here are the approximate subdirectory file sizes if you download all data files from all years:
+
+|Subdirectory|Approximate Size|
+|:--|:-:|
+|`./data`|770.5 MB|
+|`./dictionary`|16.6 MB|
+|`./sas_prog`|5.1 MB|
+|`./spss_prog`|4.9 MB|
+|`./stata_data`|673.9 MB|
+|`./stata_prog`|5.7 MB|
---
+++ downloadipeds.R
@@ -0,0 +1,135 @@
+################################################################################
+##
+## <PROJ> Batch download IPEDS files
+## <FILE> downloadipeds.R
+## <AUTH> Benjamin Skinner
+## <INIT> 21 July 2015
+## <REVN> 10 December 2016
+##
+################################################################################
+
+## PURPOSE ---------------------------------------------------------------------
+
+## Use this script to batch download IPEDS files. Only those files listed
+## in `ipeds_file_list.txt` will be downloaded. The default behavior is to
+## download each of the following files into their own subdirectories:
+##
+## (1) Data file
+## (2) Dictionary file
+##
+## You can also choose to download other data versions and/or program files:
+##
+## (1) Data file (STATA version)
+## (2) STATA program file (default if you ask for DTA version data)
+## (3) SPSS program file
+## (4) SAS program file
+##
+## The default behavior is download ALL OF IPEDS. If you don't want everything,
+## modify `ipeds_file_list.txt` to only include those files that you want.
+## Simply erase those you don't want, keeping one file name per row.
+## -----------------------------------------------------------------------------
+
+## clear
+rm(list = ls())
+
+## -----------------------------------------------------------------------------
+## CHOOSE WHAT YOU WANT (TRUE == Yes, FALSE == No)
+## -----------------------------------------------------------------------------
+
+## default
+primary_data = TRUE
+dictionary = TRUE
+
+## STATA version
+## (NB: downloading Stata version of data will also get Stata program files)
+stata_data = FALSE
+
+## other program files
+prog_spss = FALSE
+prog_sas  = FALSE
+
+## =============================================================================
+## FUNCTIONS
+## =============================================================================
+
+## message
+mess <- function(to_screen) {
+    message(rep('-',80))
+    message(to_screen)
+    message(rep('-',80))
+}
+
+## create subdirectories
+make_dir <- function(opt, dir_name) { if (opt) dir.create(dir_name) }
+
+## download file
+get_file <- function(opt, dir_name, url, file, suffix) {
+    if (opt) {
+        download.file(paste0(url, file, suffix), paste0(dir_name, file, suffix))
+    }
+}
+
+## countdown
+countdown <- function(pause, text) {
+    cat('\n')
+    for (i in pause:0) {
+        cat('\r', text, i)
+        Sys.sleep(1)
+        if (i == 0) { cat('\n\n') }
+    }
+}
+
+## =============================================================================
+## RUN
+## =============================================================================
+
+## read in files; remove blank lines
+ipeds <- readLines('./ipeds_file_list.txt')
+ipeds <- ipeds[ipeds != '']
+
+## data url
+url <- 'http://nces.ed.gov/ipeds/datacenter/data/'
+
+## create folders if they don't exist
+mess('Creating directories for downloaded files')
+make_dir(primary_data, './data')
+make_dir(stata_data, './stata_data')
+make_dir(dictionary, './dictionary')
+make_dir(stata_data, './stata_prog')
+make_dir(prog_spss, './spss_prog')
+make_dir(prog_sas, './sas_prog')
+
+## get timer (pause == max(# of options, 3))
+opts <- c(primary_data, stata_data, dictionary, prog_spss, prog_sas)
+pause <- max(length(which(opts)), 3)
+
+## loop through files
+for(i in 1:length(ipeds)) {
+
+    mess(paste0('Now downloading: ', ipeds[i]))
+
+    ## data
+    get_file(primary_data, './data/', url, ipeds[i], '.zip')
+
+    ## dictionary
+    get_file(dictionary, './dictionary/', url, ipeds[i], '_Dict.zip')
+
+    ## Stata data and program (optional)
+    get_file(stata_data, './stata_data/', url, ipeds[i], '_Data_Stata.zip')
+    get_file(stata_data, './stata_prog/', url, ipeds[i], '_Stata.zip')
+
+    ## SPSS program (optional)
+    get_file(prog_spss, './spss_prog/', url, ipeds[i], '_SPS.zip')
+
+    ## SAS program (optional)
+    get_file(prog_sas, './sas_prog/', url, ipeds[i], '_SAS.zip')
+
+    ## pause and countdown
+    countdown(pause, 'Give IPEDS a little break ...')
+}
+
+mess('\nFinished!')
+
+## =============================================================================
+## END
+################################################################################
